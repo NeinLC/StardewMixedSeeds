f58221bfd56175ffe86301522adffe40275af044
NeinLC <165825799+NeinLC@users.noreply.github.com>
2024-04-12 22:49:42 -0600

    Update ModEntry.cs
    - Added functions for Winter Randomizing, Randomizing Ginger Island, Toggle trellis crops, and toggle all flowers moving to mixed flower seeds
    - Added function to read information from json files, easier for users to edit settings and easier to add new mod compatibility

--- ModEntry.cs
+++ ModEntry.cs
@@ -11,16 +11,35 @@ using xTile.Format;
 using Microsoft.VisualBasic;
 using StardewValley.Locations;
 using StardewValley.Extensions;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using StardewValley.Menus;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using StardewValley.GameData.Shops;
 
 namespace SeedMod
 {
+    enum MixType
+    {
+        Crops,
+        Flowers
+    }
+
     /// <summary>The mod entry point.</summary>
     internal sealed class ModEntry : Mod
-    {        
+    {
+
+        // Used to hold the mod folder directory        
+        public static string path= "";     
         
         public override void Entry(IModHelper helper)
-        {   
-                                   
+        { 
+
+            // Gets the path using SMAPI helper                        
+            path = this.Helper.DirectoryPath;
+                                               
             var harmony = new Harmony(this.ModManifest.UniqueID);           
 
             // Gets the season for use in postfix
@@ -35,18 +54,46 @@ namespace SeedMod
                 postfix: new HarmonyMethod(typeof(SeedPatch), nameof(SeedPatch.Postfix))
             );
 
+        }  
+
+        // Hold the mod setting information
+        public class ModConfig
+        {
+            public bool RandomizeWinter { get; set; }
+            public bool TrellisEnabled { get; set; }
+            public bool FlowersInFlowerMix { get; set; }
+            public bool RandomizeGingerIsland { get; set; }
+        }     
+
+        // Hold information settings for each modset of seeds        
+        public class SeedSet
+        {
+            public string name {get; set;}
+            public bool enabled {get; set;}
+            public double dropChance {get; set;}               
         }
 
+        // Holds seed information
         public class Seed
         {
             public string cropName { get; set; } = null!;
             public string seedID { get; set; } = null!;
             public double dropChance { get; set; }
+            public bool enabled { get; set; }
+            public bool isTrellis {get; set;}
+            public bool isFlower {get; set;}
         }
 
 
         public class SeedPatch
         {
+
+            // Gets the config for the game from config.json
+            static string ConfigPath = Path.Combine(path, "config.json");                     
+            static JObject configJson = JObject.Parse(File.ReadAllText(ConfigPath));
+            static JToken jConfig = configJson["ModSettings"];
+            static ModConfig config = jConfig.ToObject<ModConfig>(); 
+
 
             // Gets the game location for use in postfix
             public static void Prefix(out GameLocation __state)
@@ -55,82 +102,142 @@ namespace SeedMod
             }
 
 
-            // Returns the list of seeds for the game season
-            public static List<Seed> GetSeedList(Season season)
+            // Method to get the seed sets in config
+            public static string GetSeedSet()
             {
+                //Create a seed set list to get all possible seed sets               
+                List<SeedSet> seedSets = new List<SeedSet>();                               
 
-                // Lists of seeds and assigned drop chance, numbers are ratios to each other.
-                // TO DO: Figure out how to call lists from a config file that can be edited easily
+                // serialize JSON results into .NET objects (seed sets)
+                IList<JToken> results = configJson["SeedSets"].Children().ToList();
 
-                List<Seed> springSeeds = new List<Seed>
-                {                    
-                    new Seed {cropName = "Parsnip", seedID = "472", dropChance = 8},
-                    new Seed {cropName = "Green Bean", seedID = "473", dropChance = 4},
-                    new Seed {cropName = "Cauliflower", seedID = "474", dropChance = 4},
-                    new Seed {cropName = "Potato", seedID = "475", dropChance = 6},
-                    new Seed {cropName = "Garlic", seedID = "476", dropChance = 1},
-                    new Seed {cropName = "Rice", seedID = "273", dropChance = 4},
-                    new Seed {cropName = "Kale", seedID = "477", dropChance = 5},
-                    new Seed {cropName = "Rhubarb", seedID = "478", dropChance = 2},
-                    new Seed {cropName = "Strawberry", seedID = "745", dropChance = 2},
-                    new Seed {cropName = "Coffee", seedID = "433", dropChance = 0.2}
-                };
-
-                List<Seed> summerSeeds = new List<Seed>
+                foreach (JToken result in results)
                 {
-                    //new Seed {cropName = "Taro Root", seedID = "433", dropChance = 0.2},                    
-                    new Seed {cropName = "Melon", seedID = "479", dropChance = 6},
-                    new Seed {cropName = "Tomato", seedID = "480", dropChance = 8},
-                    new Seed {cropName = "Blueberry", seedID = "481", dropChance = 6},
-                    new Seed {cropName = "Hot Pepper", seedID = "482", dropChance = 8},
-                    new Seed {cropName = "Wheat", seedID = "483", dropChance = 12},
-                    new Seed {cropName = "Radish", seedID = "484", dropChance = 4},
-                    new Seed {cropName = "Red Cabbage", seedID = "485", dropChance = 1},
-                    new Seed {cropName = "Starfruit", seedID = "486", dropChance = 1},
-                    //new Seed {cropName = "Pineapple", seedID = "745", dropChance = 2},
-                    new Seed {cropName = "Coffee", seedID = "433", dropChance = 0.2},
-                    new Seed {cropName = "Cactus Fruit", seedID = "433", dropChance = 2},
-                    new Seed {cropName = "Corn", seedID = "487", dropChance = 6},                    
-                    new Seed {cropName = "Hops", seedID = "302", dropChance = 4},
-                    //new Seed {cropName = "Ancient Fruit", seedID = "499", dropChance = 0.2},                    
-                };
+                    // JToken.ToObject is a helper method that uses JsonSerializer internally
+                    SeedSet seedSet = result.ToObject<SeedSet>();
+                    if (seedSet.enabled)
+                    {
+                        seedSets.Add(seedSet);
+                    }                    
+                } 
 
-                List<Seed> fallSeeds = new List<Seed>
-                {                    
-                    new Seed {cropName = "Wheat", seedID = "483", dropChance = 12},
-                    new Seed {cropName = "Corn", seedID = "487", dropChance = 8},
-                    new Seed {cropName = "Grape", seedID = "301", dropChance = 6},
-                    new Seed {cropName = "Amaranth", seedID = "299", dropChance = 6},
-                    new Seed {cropName = "Eggplant", seedID = "488", dropChance = 6},
-                    new Seed {cropName = "Artichoke", seedID = "489", dropChance = 4},
-                    new Seed {cropName = "Pumpkin", seedID = "490", dropChance = 4},
-                    new Seed {cropName = "Bok Choy", seedID = "491", dropChance = 8},                    
-                    new Seed {cropName = "Yam", seedID = "492", dropChance = 6},                    
-                    new Seed {cropName = "Cranberries", seedID = "493", dropChance = 6},
-                    new Seed {cropName = "Beet", seedID = "494", dropChance = 4},
-                    //new Seed {cropName = "Ancient Fruit", seedID = "499", dropChance = 0.2},
-                    //new Seed {cropName = "SweetGemBerry", seedID = "302", dropChance = 4},  
-                                   
-                };
-
-                List<Seed> winterSeeds = new List<Seed>
-                {                    
-                    new Seed {cropName = "Powdermelons", seedID = "Powdermelon Seeds", dropChance = 1},                                      
-                };
-
-                // Return list based on the season, winter as default                
-                switch (season)
-                {
-                    case Season.Spring: return springSeeds;
-                    case Season.Summer: return summerSeeds;
-                    case Season.Fall: return fallSeeds;
-                    default: return winterSeeds;
-                }                             
-
+                // Call get weighted set to determine which set will be pulled from              
+                string seedSetName = GetWeightedSet(seedSets);
+                return seedSetName;   
             }
 
-            // This is just directly from the game
-            public static string getRandomFlowerSeedForThisSeason(Season season)
+            // Returns a seed set name to pull a list from
+            public static string GetWeightedSet(List<SeedSet> list)
+            {
+                Random rand = new Random();
+                double totalWeight = 0;
+                string setName = ""; 
+
+                // Gets a total from drop chance to compare drop chances against
+                foreach (SeedSet item in list){
+                    totalWeight += item.dropChance;              
+                }
+
+                // Selects a seed set randomly with weighted probability from drop chance
+                double r = rand.NextDouble() * totalWeight;
+                double sum = 0;
+                foreach(SeedSet set in list)
+                {
+                    if(r <= (sum = sum + set.dropChance)){
+                        setName = set.name;                        
+                        return setName;
+                    }
+                }
+
+                return setName;                   
+            }
+
+            // Returns a string Seed ID after selecting a seed using the drop chance ratios
+            public static string GetWeightedSeed(List<Seed> seedList)
+            {
+                Random rand = new Random();
+                double totalWeight = 0;
+                string seedID = "";   
+
+                // Gets a total from drop chance to compare drop chances against
+                foreach (Seed seed in seedList){
+                    totalWeight += seed.dropChance;              
+                }
+
+                // Selects a seed ID randomly with weighted probability from drop chance
+                double r = rand.NextDouble() * totalWeight;
+                double sum = 0;
+                foreach(Seed seed in seedList)
+                {
+                    if(r <= (sum = sum + seed.dropChance)){
+                        seedID = seed.seedID;
+                        break;
+                    }
+                } 
+                return seedID;               
+            }
+
+
+            // Returns the list of seeds for the game season
+            public static List<Seed> GetSeedList(Season season, MixType mix)
+            {             
+                // Get season name as a string to navigate json file
+                string seasonName = season.ToString();
+
+                //Create a seed list to store seeds                
+                List<Seed> seeds = new List<Seed>();
+
+                //Counter variable for tries
+                int attempts = 5;
+
+                // Limit the number of tries before returning blank list, in case
+                // user restrictions make seed finding difficult
+                while (seeds.Count == 0 && attempts > 0)
+                {
+                    // Get the seedset this seed will pull from
+                    string seedSet = GetSeedSet() + ".json";
+
+                    //Get the path to the json file being used                
+                    string seedPath = Path.Combine(path, "SeedSets", seedSet);       
+                    
+                    //Read the json file information, store as json
+                    JObject json = JObject.Parse(File.ReadAllText(seedPath));                    
+
+                    // serialize JSON results into .NET objects, gets seeds for that seaon
+                    IList<JToken> results = json[seasonName].Children().ToList();
+                    
+                    foreach (JToken result in results)
+                    {
+                        // JToken.ToObject is a helper method that uses JsonSerializer internally
+                        Seed seed = result.ToObject<Seed>();
+                        if (seed.enabled)
+                        {
+                            //Excludes trellis crops if trellis is disabled
+                            if (!seed.isTrellis || config.TrellisEnabled)
+                            { 
+                                // For regular mixed seeds                                                               
+                                if (mix == MixType.Crops && (!(seed.isFlower) || !(config.FlowersInFlowerMix)))
+                                {
+                                    seeds.Add(seed);
+                                }
+
+                                // GetSeedList only called when FlowersinFlowerMix enabled (for mixed flower seeds)
+                                if (mix == MixType.Flowers && seed.isFlower)
+                                {
+                                    seeds.Add(seed);
+                                }
+                            }                        
+                        }                    
+                    } 
+                    //Decrement attempts to be made
+                    attempts--;           
+                }                
+                return seeds;  
+            }
+
+
+            // The vanilla mixed flower seeds method, copied from the game
+            public static string getVanillaFlowerMix(Season season)
             {
                 if (season == Season.Winter)
                 {
@@ -145,61 +252,76 @@ namespace SeedMod
                 };
             }
 
-
+            // This method can edit the result of the game method ResolveSeedId
             public static void Postfix(string itemId, GameLocation __state, ref string __result)
-            {
-                
-                             
+            {  
+                // Gets the ingame season                             
                 GameLocation currentLocation = __state;
-                
+                Season season = currentLocation.GetSeason();               
 
+                // Randomize seed season for winter or ginger island if config settings apply
+                if ((__state is IslandLocation && config.RandomizeGingerIsland) || (season == Season.Winter && config.RandomizeWinter))
+                {
+                    season = Game1.random.Choose(Season.Spring, Season.Summer, Season.Fall);                        
+                }  
+
+                // Gets seeds for Mixed Flower Seeds
                 if (itemId == "MixedFlowerSeeds")
                 {
-                    __result = getRandomFlowerSeedForThisSeason(currentLocation.GetSeason());
+                    if(config.FlowersInFlowerMix)
+                    {                        
+                        // Get a flower seedlist for the current location's season
+                        List<Seed> seedList = GetSeedList(season, MixType.Flowers); 
 
-                }
-
-                else if (itemId == "770")
-                {
-                    // Get a seedlist for the current location's season
-                    List<Seed> seedList = GetSeedList(currentLocation.GetSeason()); 
-
-                    Random rand = new Random();
-                    double totalWeight = 0;
-                    string seedID = ""; 
-
-                    // Gets a total from drop chance to compare drop chances against
-                    foreach (Seed seed in seedList){
-                        totalWeight += seed.dropChance;              
-                    }
-
-                    // Selects a seed ID randomly with weighted probability from drop chance
-                    double r = rand.NextDouble() * totalWeight;
-                    double sum = 0;
-                    foreach(Seed seed in seedList)
-                    {
-                        if(r <= (sum = sum + seed.dropChance)){
-                            seedID = seed.seedID;
-                            break;
-                        }
-                    }
-                                             
-                    // For the island location in game, not dependent on season
-                    // This is copied from game code
-                    if (__state is IslandLocation)
-                    {
-                        seedID = Game1.random.Next(4) switch
+                        // if seedlist isn't empty (prevent null return errors)
+                        if (seedList.Count > 0)
                         {
-                            0 => "479", 
-                            1 => "833", 
-                            2 => "481", 
-                            _ => "478", 
-                        };
+                            // Get a weighted seed selection and set result
+                            string seedID = GetWeightedSeed(seedList);
+                            __result = seedID;
+                        }                 
                     }
-                    __result = seedID;                        
+                    // Use vanilla forumla if FlowersinFlowerMixed disabled
+                    else
+                    {
+                        __result = getVanillaFlowerMix(season);
+                    }
                 }
 
-                // Anything not a mixed seed just returns it's ID
+                // Gets seeds for mixed seeds
+                else if (itemId == "770")
+                {                                     
+
+                    // Get a crops seedlist for the current location's season
+                    List<Seed> seedList = GetSeedList(season, MixType.Crops); 
+
+                    // if seedlist isn't empty
+                    if (seedList.Count > 0)
+                    {
+                        // Select a weighted seed for the seedID
+                        string seedID = GetWeightedSeed(seedList);
+                                                
+                        // Vanilla ginger island if ginger island not randomized
+                        if (__state is IslandLocation && !config.RandomizeGingerIsland)
+                        {                            
+                            seedID = Game1.random.Next(4) switch
+                            {
+                                0 => "479", 
+                                1 => "833", 
+                                2 => "481", 
+                                _ => "478", 
+                            };
+                        }
+                        __result = seedID;
+                    }  
+                    else
+                    {
+                        //TODO: Log error message?
+                        //Will not change result if functions failed, default to vanilla
+                    } 
+                }
+
+                // Anything not a mixed seed just returns its ID
                 else 
                 {
                     __result = itemId;
